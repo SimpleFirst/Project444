ef99eb73379fcdfd70b5b071d7c79e3f1a0ca432
tm.boberr@gmail.com <tm.boberr@gmail.com>
2018-07-03 17:00:21 +0300

    .

--- src/FirsProjectGit/Main.java
+++ src/FirsProjectGit/Main.java
@@ -3,5 +3,6 @@ package FirsProjectGit;
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello FirstProjectGit");
+        System.out.println("Изменения вносим ");
     }
 }
